8b7eefbab13f2e70fe9c85bdc3ca86b9d195e321
Arek Ouzounian <agouzo777@gmail.com>
2020-10-16 19:36:49 -0700

    completely reworked roll command: you can now add, subtract, multiply, and divide your rolls. Rolls now use custom embeds rather than plain text.

--- dnd-bot/Commands.cs
+++ dnd-bot/Commands.cs
@@ -1,11 +1,10 @@
 ﻿using Discord.Commands;
 using System;
 using Discord;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.WebSocket;
+using Dangl.Calculator;
 
 namespace dnd_bot
 {
@@ -15,15 +14,117 @@ namespace dnd_bot
         public theStatHandler statHandler = new theStatHandler();
 
         [Command("roll")]
-        public async Task roll(string rollCode)
+        public async Task roll(params string[] args)
         {
+            string rollCode = args[0];
+            string calculations = "";
+            if (args.Length == 1)
+            {
+                foreach(var character in args[0])
+                {
+                    if(character > 41 && character < 48 && character != 44 && character != 46) 
+                    {
+                        string[] arr = new string[2];
+                        int index = args[0].IndexOf(character);
+                        arr[0] = args[0].Substring(0, index);
+                        arr[1] = args[0].Substring(index);
+                        rollCode = arr[0];
+                        calculations = arr[1];
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                StringBuilder strB = new StringBuilder();
+                foreach (var item in args)
+                {
+                    item.Replace(" ", "");
+                    strB.Append(item);
+                }
+                await roll(strB.ToString());
+                return;
+            }
             Random gen = new Random();
-            await Context.Channel.SendMessageAsync($"Rolling {rollCode}...");
+            await Context.Channel.SendMessageAsync($"Rolling {rollCode} {calculations}...");
             int num;
             if(int.TryParse(rollCode, out num))
             {
+                var sum = Calculator.Calculate($"{num}{calculations}").Result;
+
+                await Context.Channel.SendMessageAsync($"You Rolled {num} {calculations} = {sum}");
+            }
+            else
+            {
+                var parsedRollCode = rollCode.Split('d');
+                int numberOfRolls = 0;
+                int sidedDie = 0;
+                if(parsedRollCode.Length == 1)
+                {
+                    numberOfRolls = 1;
+                    sidedDie = int.Parse(parsedRollCode[0]);
+                }
+                else if(parsedRollCode.Length == 2)
+                {
+                    numberOfRolls = int.Parse(parsedRollCode[0]);
+                    sidedDie = int.Parse(parsedRollCode[1]);
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync("The command was inputted incorrectly. Try doing: /roll [number of dice to roll]d[number of sides on each dice] [operation] [modifier]");
+                    return;
+                }
+
+                if(numberOfRolls > 500 || numberOfRolls < 1)
+                {
+                    await Context.Channel.SendMessageAsync("Incorrect amount of dice. You can only roll up to 500 dice at a time.");
+                    return;
+                }
+
+                StringBuilder strB = new StringBuilder();
+                int sum = 0; 
+                for(int i = 0; i < numberOfRolls; i++)
+                {
+                    int roll = gen.Next(1, sidedDie + 1);
+                    strB.Append($"{roll}");
+                    if (i < numberOfRolls - 1)
+                    {
+                        strB.Append(" + ");
+                    }
+                    sum += roll;
+                }
+                strB.Append(" " + calculations);
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.WithTitle("Dice Roller");
+                eb.WithAuthor(Context.User);
+                eb.WithColor(getUserColor(Context.User as SocketGuildUser));
+                var charLimit = 1900;
+                if (strB.ToString().Length > charLimit)
+                {
+                    var amtOfMsgs = Math.Ceiling((double)(strB.Length / charLimit));
+                    eb.AddField("Notice!", "This is a very large roll that surpasses the character limit, so I have to break it up into parts.");
+                    for (int i = 0; i < amtOfMsgs; i++)
+                    {
+                        var substring = strB.ToString().Substring(i * charLimit, charLimit);
+                        eb.AddField($"Rolls Part {i}", substring);
+                    }
+                }
+                else
+                {
+                    eb.AddField("Rolls: ", strB.ToString());
+                }
+                eb.AddField("Total: ", Calculator.Calculate($"{sum}{calculations}").Result);
+
+                await Context.Channel.SendMessageAsync(null, false, eb.Build());
+            }
+            
+            #region shitCode
+            /*
+            int num;
+            if (int.TryParse(rollCode, out num))
+            {
                 await Context.Channel.SendMessageAsync(Format.Code($"You Rolled {num}."));
-                if(num == 17)
+                if (num == 17)
                 {
                     await Context.Channel.SendMessageAsync(Format.Bold(Format.Italics("That doesn't hit.")));
                 }
@@ -32,27 +133,27 @@ namespace dnd_bot
             else
             {
                 var nums = rollCode.Split('d');
-                if(nums.Length != 2)
+                if (nums.Length != 2)
                 {
-                    await Context.Channel.SendMessageAsync($"That is an invalid amount of dice. The amount should look like this: [number of dice]d[number of sides on each given die]");
+                    await Context.Channel.SendMessageAsync($"That is an invalid amount of dice. The amount should look like this: [number of dice]d[number of sides on each given die] (+ or -) [modifier]");
                     return;
                 }
-                if(int.Parse(nums[0]) > 500)
+                if (int.Parse(nums[0]) > 500)
                 {
                     await Context.Channel.SendMessageAsync("That is too many dice! You can only roll up to 500 dice at a time.");
                     return;
                 }
 
-                int[] rolls = new int[int.Parse(nums[0])]; 
-                for(int i = 0; i < rolls.Length; i++)
+                int[] rolls = new int[int.Parse(nums[0])];
+                for (int i = 0; i < rolls.Length; i++)
                 {
                     rolls[i] = gen.Next(1, int.Parse(nums[1]) + 1);
                 }
                 StringBuilder strB = new StringBuilder();
                 int sum = 0;
-                for(int i = 0; i < rolls.Length; i++)
+                for (int i = 0; i < rolls.Length; i++)
                 {
-                    if(i == rolls.Length - 1)
+                    if (i == rolls.Length - 1 && modifier == 0)
                     {
                         strB.Append(rolls[i]);
                     }
@@ -62,25 +163,46 @@ namespace dnd_bot
                     }
                     sum += rolls[i];
                 }
-                var charLimit = 1900;
-                if(rolls.Length > 1)
+                if (modifier != 0)
                 {
-                    if(strB.ToString().Length > charLimit)
+                    if (operation == '+')
                     {
-                        var amtOfMsgs = Math.Ceiling((double)(strB.Length / charLimit));    //(double)strB.ToString().Length / charLimit > (int)strB.ToString().Length / charLimit ? (int)strB.ToString().Length / charLimit + 1 : (int)strB.ToString().Length / charLimit;
-                        for(int i = 0; i < amtOfMsgs; i++)
-                        {
-                            var substring = Format.Code(strB.ToString().Substring(i * charLimit, charLimit));
-                            await Context.Channel.SendMessageAsync(substring);
-                        }
+                        strB.Append($" {operation} {modifier}");
+                        sum += modifier;
                     }
-                    else
+                    else if (operation == '-')
                     {
-                        await Context.Channel.SendMessageAsync(Format.Code($"{strB.ToString()}"));
+                        strB.Append($" {operation} {modifier}");
+                        sum -= modifier;
                     }
-                    
+                    else if (operation == '/')
+                    {
+                        strB.Append($" {operation} {modifier}");
+                        sum /= modifier;
+                    }
+                    else if (operation == '*')
+                    {
+                        strB.Append($" {operation} {modifier}");
+                        sum *= modifier;
+                    }
                 }
-                await Context.Channel.SendMessageAsync(Format.Code($"You Rolled {sum}."));
+                var charLimit = 1900;
+
+                if (strB.ToString().Length > charLimit)
+                {
+                    var amtOfMsgs = Math.Ceiling((double)(strB.Length / charLimit));
+                    for (int i = 0; i < amtOfMsgs; i++)
+                    {
+                        var substring = Format.Code(strB.ToString().Substring(i * charLimit, charLimit));
+                        await Context.Channel.SendMessageAsync(substring);
+                    }
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync(Format.Code($"{strB.ToString()} {operation} {modifier}"));
+                }
+
+                await Context.Channel.SendMessageAsync($"Total: {sum}");
 
                 if (int.Parse(nums[1]) == 20)
                 {
@@ -102,7 +224,7 @@ namespace dnd_bot
                             {
                                 failCount++;
                             }
-                            else if(roll == 20)
+                            else if (roll == 20)
                             {
                                 winCount++;
                             }
@@ -110,7 +232,20 @@ namespace dnd_bot
                         await Context.Channel.SendMessageAsync($"You rolled {winCount} natural 20s and {failCount} natural 1s!");
                     }
                 }
+            }*/
+            #endregion
+        }
+        public Color getUserColor(SocketGuildUser user)
+        {
+            foreach(var role in user.Roles)
+            {
+                Console.WriteLine(role.Color);
+                if (role.Id != 738556835036135467 && role.Id != 738549927537410048 && !user.IsBot)
+                {
+                    return role.Color;
+                }
             }
+            return Color.Default;
         }
 
         [Command("test")]
@@ -121,20 +256,29 @@ namespace dnd_bot
         }
 
         [Command("spell")]
-        public async Task findSpell(string spellName)
+        public async Task findSpell(params string[] spellName)
         {
-            SpellHelper spelper = new SpellHelper(spellName);
+            StringBuilder strB = new StringBuilder();
+            foreach (var word in spellName)
+            {
+                strB.Append($"{word} ");
+            }
+            SpellHelper spelper = new SpellHelper(strB.ToString());
             spelper.printSpell(Context);
 
         }
 
         [Command("monster")]
         //[RequireUserPermission(GuildPermission.Administrator)]
-        public async Task findMonster(string monsterName)
+        public async Task findMonster(params string[] monsterName)
         {
-            MonsterHelper melper = new MonsterHelper(monsterName);
+            StringBuilder strB = new StringBuilder();
+            foreach (var word in monsterName)
+            {
+                strB.Append($"{word} ");
+            }
+            MonsterHelper melper = new MonsterHelper(strB.ToString());
             melper.printMonster(Context);
-            
         }
 
         [Command("help"), Alias("Help")]
@@ -150,13 +294,13 @@ namespace dnd_bot
         {
             statHandler.rollForTheStat();
         }
-        
+
         [Command("addweapon")]
         //[RequireOwner]
         public async Task addWeapon(string name, string damage, string damageType, string effects)
         {
             var weaponAdded = welper.AddWeapon(name, damage, damageType, effects, Context.User.Id);
-            if(weaponAdded)
+            if (weaponAdded)
             {
                 await Context.Channel.SendMessageAsync("Weapon Logged Successfully.");
             }
@@ -166,20 +310,20 @@ namespace dnd_bot
             }
         }
 
-        [Command("getweapons")] 
+        [Command("getweapons")]
         //[RequireOwner]
         public async Task getWeapons()
         {
             var weaponList = welper.GetWeapons();
-            if(weaponList.Weapons.Count > 0)
+            if (weaponList.Weapons.Count > 0)
             {
                 EmbedBuilder eb = new EmbedBuilder();
                 eb.WithTitle($"{Context.User.Username}'s Weapons");
                 eb.WithColor(Color.DarkGrey);
                 int weaponCount = 0;
-                foreach(var weapon in weaponList.Weapons)
+                foreach (var weapon in weaponList.Weapons)
                 {
-                    if(weapon.OwnerID == Context.User.Id)
+                    if (weapon.OwnerID == Context.User.Id)
                     {
                         eb.AddField(weapon.Name, $"Damage Dice: {weapon.DamageDice}" +
                         $"\nDamage Type: {weapon.DamageType}" +
@@ -187,7 +331,7 @@ namespace dnd_bot
                         weaponCount++;
                     }
                 }
-                if(weaponCount == 0)
+                if (weaponCount == 0)
                 {
                     eb.AddField("No Weapons Here.", "Please use the 'addweapon' command to add your weapons!");
                 }
@@ -242,7 +386,7 @@ namespace dnd_bot
         public async Task rollDamage(string weaponName)
         {
             bool weaponRolled = false;
-            foreach(var weapon in welper.GetWeapons().Weapons)
+            foreach (var weapon in welper.GetWeapons().Weapons)
             {
                 if (weapon.Name.ToLower() == weaponName.ToLower() && weapon.OwnerID == Context.User.Id)
                 {
@@ -250,7 +394,7 @@ namespace dnd_bot
                     weaponRolled = true;
                 }
             }
-            if(!weaponRolled)
+            if (!weaponRolled)
             {
                 await Context.Channel.SendMessageAsync("You don't have that weapon.");
             }
@@ -259,7 +403,7 @@ namespace dnd_bot
         [Command("statAvg"), Alias("statAnvg")]
         public async Task getStatAvg()
         {
-            if(statHandler.getCareerAvg(Context.User.Id) == -1)
+            if (statHandler.getCareerAvg(Context.User.Id) == -1)
             {
                 await Context.Channel.SendMessageAsync($"I don't have your career average stored yet. Once {Format.Bold("the stat")} has been rolled more, I can compute your average.");
             }
@@ -293,33 +437,6 @@ namespace dnd_bot
             }
 
             await removeGivenWeapon(weaponName, user);
-        }
-
-        [Command("dmMode")]
-        [RequireOwner]
-        public async Task DmMode()
-        {
-            await Context.Channel.DeleteMessageAsync(Context.Message);
-            bool isDone = false;
-
-            while(!isDone)
-            {
-                var msg = Console.ReadLine();
-
-                if(msg == "terminate")
-                {
-                    isDone = true;
-                    continue;
-                }
-                else if(msg == "")
-                {
-                    continue;
-                }
-                else
-                {
-                    await Context.Channel.SendMessageAsync(msg);
-                }
-            }
         }
         private async Task removeGivenWeapon(string weaponName, SocketUser user)
         {
